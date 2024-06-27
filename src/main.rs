88401578eccbd196bd14c68c47db79681fd6d0c2
Konstantinos K <karachris@csd.auth.gr>
2024-06-27 23:58:02 +0300

    feat: get color from base64 string

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,23 @@ fn main() {
 
     let base64_representation: String = URL_SAFE.encode(input.as_bytes());
     println!("{} / {}", base64_representation, input);
+
+    let color = get_color(input);
+    println!("color: {:?}", color);
+}
+
+fn get_color(input: String) -> Color {
+    let input_as_u8 = input.as_bytes();
+    Color {
+        red: input_as_u8[0],
+        green: input_as_u8[1],
+        blue: input_as_u8[2],
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
 }
