01d8bb10c003bcc8af1c4465a0f20b385770b2a2
Konstantinos K <karachris@csd.auth.gr>
2024-06-27 23:57:37 +0300

    feat: convert string to base64

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use base64::{engine::general_purpose::URL_SAFE, Engine as _};
+
 fn main() {
-    println!("Hello, world!");
+    let input = String::from("konstantinos.karachristos");
+
+    let base64_representation: String = URL_SAFE.encode(input.as_bytes());
+    println!("{} / {}", base64_representation, input);
 }
